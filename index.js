bd975444abd63a917eb0489422e60ff644377204
Jon Winton <winton.jon@gmail.com>
2017-04-17 16:41:44 -0400

    Hooking up indexing logic from the Clay implementation

--- index.js
+++ index.js
@@ -3,3 +3,7 @@
 module.exports.setup = require('./lib/setup');
 module.exports.init = require('./lib/init');
 module.exports.save = require('./lib/save');
+
+module.exports.elastic = require('./lib/services/elastic');
+module.exports.helpers = require('./lib/services/elastic-helpers');
+module.exports.filters = require('./lib/services/filters');
