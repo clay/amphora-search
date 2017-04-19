b4b9b01f1934d1151cb5e50e2681bf864786f81f
Jon Winton <winton.jon@gmail.com>
2017-04-19 11:54:48 -0400

    Adding in an initial _search route for testing plugin functionality

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 'use strict';
 
+// Setup the ES client and other startup tasks
 module.exports.setup = require('./lib/setup');
+
+// Hooks
+module.exports.routes = require('./lib/routes');
 module.exports.init = require('./lib/init');
 module.exports.save = require('./lib/save');
 
+// Export helper functions for indices in a Clay instance
 module.exports.elastic = require('./lib/services/elastic');
 module.exports.helpers = require('./lib/services/elastic-helpers');
 module.exports.filters = require('./lib/services/filters');
