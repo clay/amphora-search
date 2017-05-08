25ab88a692b57faa16f81be04882a65c845428d8
Jon Winton <winton.jon@gmail.com>
2017-05-08 14:29:27 -0400

    Adding a `getInstance` function per #13

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const elastic = require('./lib/services/elastic');
+
 // Setup the ES client and other startup tasks
 module.exports.setup = require('./lib/setup');
 
@@ -9,7 +11,8 @@ module.exports.init = require('./lib/init');
 module.exports.save = require('./lib/save');
 
 // Export helper functions for indices in a Clay instance
-module.exports.elastic = require('./lib/services/elastic');
+module.exports.elastic = elastic;
+module.exports.getInstance = elastic.getInstance;
 module.exports.helpers = require('./lib/services/elastic-helpers');
 module.exports.filters = require('./lib/services/filters');
 
