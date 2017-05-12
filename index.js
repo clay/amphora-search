9ce5a266fbbae127fdf4a63919b725e6fc876b09
Jon Winton <winton.jon@gmail.com>
2017-05-12 12:07:04 -0400

    Adding a prefix and stripping function to make that more consistent

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const elastic = require('./lib/services/elastic');
+const elastic = require('./lib/services/elastic'),
+  helpers = require('./lib/services/elastic-helpers');
 
 // Setup the ES client and other startup tasks
 module.exports.setup = require('./lib/setup');
@@ -13,8 +14,10 @@ module.exports.save = require('./lib/save');
 // Export helper functions for indices in a Clay instance
 module.exports.elastic = elastic;
 module.exports.getInstance = elastic.getInstance;
-module.exports.helpers = require('./lib/services/elastic-helpers');
+module.exports.helpers = helpers;
 module.exports.filters = require('./lib/services/filters');
 
-// Page List
+// Page List & Other Helpers
 module.exports.updatePageProperty = require('./lib/page-list').updatePageEntry;
+module.exports.stripPrefix = helpers.stripPrefix;
+module.exports.indexWithPrefix = helpers.indexWithPrefix;
