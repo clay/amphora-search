d18a4e83c80d8ef7aa745a314404f7a9c4b8dcb0
Nelson Pecora <nelson@yoshokatana.com>
2018-01-24 16:50:50 -0500

    first pass of page list refactor

--- index.js
+++ index.js
@@ -17,6 +17,12 @@ module.exports.save = require('./lib/save');
 module.exports.delete = require('./lib/delete');
 module.exports.publish = require('./lib/publish');
 module.exports.unpublish = require('./lib/unpublish');
+// Page List Hooks
+module.exports.createPage = require('./lib/page-list/create');
+module.exports.publishPage = require('./lib/page-list/publish');
+module.exports.unpublishPage = require('./lib/page-list/unpublish');
+module.exports.schedulePage = require('./lib/page-list/schedule');
+module.exports.unschedulePage = require('./lib/page-list/unschedule');
 
 // Export helper functions for indices in a Clay instance
 module.exports.elastic = elastic;
