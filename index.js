f8aa5ac7848afce3c01adc0762ff698ac5c56a7b
Jon Winton <winton.jon@gmail.com>
2018-07-02 14:32:12 -0400

    initial bus

--- index.js
+++ index.js
@@ -9,22 +9,20 @@ module.exports.setup = require('./lib/setup');
 
 // Hooks
 module.exports.subscribe = streams.subscribe;
-module.exports.publishStream = streams.publishStream;
-module.exports.saveStream = streams.saveStream;
 module.exports.routes = require('./lib/routes');
 module.exports.init = require('./lib/init');
-module.exports.save = require('./lib/save');
-module.exports.delete = require('./lib/delete');
-module.exports.publish = require('./lib/publish');
-module.exports.unpublish = require('./lib/unpublish');
+// module.exports.save = require('./lib/save');
+// module.exports.delete = require('./lib/delete');
+
+// module.exports.unpublish = require('./lib/unpublish');
 // Page List Hooks
-module.exports.createPage = require('./lib/page-list/create');
-module.exports.publishPage = require('./lib/page-list/publish');
-module.exports.unpublishPage = require('./lib/page-list/unpublish');
-module.exports.schedulePage = require('./lib/page-list/schedule');
-module.exports.unschedulePage = require('./lib/page-list/unschedule');
-// Layout List Hooks
-module.exports.publishLayout = require('./lib/layout-list/publish');
+// module.exports.createPage = require('./lib/page-list/create');
+
+// module.exports.unpublishPage = require('./lib/page-list/unpublish');
+// module.exports.schedulePage = require('./lib/page-list/schedule');
+// module.exports.unschedulePage = require('./lib/page-list/unschedule');
+// // Layout List Hooks
+// module.exports.publishLayout = require('./lib/layout-list/publish');
 
 // Export helper functions for indices in a Clay instance
 module.exports.elastic = elastic;
