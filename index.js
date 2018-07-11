30184c264338b1dac24985adade69aef2c8d577f
Jon Winton <winton.jon@gmail.com>
2018-07-11 12:20:06 -0400

    stashing changes

--- index.js
+++ index.js
@@ -4,21 +4,12 @@ const elastic = require('./lib/services/elastic'),
   helpers = require('./lib/services/elastic-helpers'),
   streams = require('./lib/streams');
 
+// Initialize the module
+module.exports = require('./lib/init');
 // Setup the ES client and other startup tasks
 module.exports.setup = require('./lib/setup');
-
-// Hooks
+// Allow subscription to Streams
 module.exports.subscribe = streams.subscribe;
-module.exports.routes = require('./lib/routes');
-module.exports.init = require('./lib/init');
-
-// Need to require these somewhere to register listeners
-require('./lib/layout-list/publish');
-require('./lib/page-list/create');
-require('./lib/page-list/publish');
-require('./lib/page-list/unpublish');
-require('./lib/page-list/schedule');
-require('./lib/page-list/unschedule');
 
 // Export helper functions for indices in a Clay instance
 module.exports.elastic = elastic;
@@ -29,3 +20,9 @@ module.exports.filters = require('./lib/services/filters');
 // Page List & Other Helpers
 module.exports.stripPrefix = helpers.stripPrefix;
 module.exports.indexWithPrefix = helpers.indexWithPrefix;
+
+// Need to require these somewhere to register listeners
+require('./lib/layout-list/publish');
+
+// require('./lib/page-list/schedule');
+// require('./lib/page-list/unschedule');
